38a89dd1d961ca701eefd5b82744e575b443ccd2
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-03-25 15:37:13 +0200

    Sain ühenduse andmebaasiga, mida kinnitas test wooohhoooooo

--- PrjDatabaseComponent/CDatabase.vb
+++ PrjDatabaseComponent/CDatabase.vb
@@ -1,3 +1,14 @@
-﻿Public Class CDatabase
+﻿Imports MySql.Data.MySqlClient
+Public Class CDatabase
+    Function connect() As Boolean
+        Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;"
+        Dim conn As New MySqlConnection(connString)
+        Try
+            conn.Open()
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
 
 End Class
---
+++ PrjDatabaseTestid/CDatabaseTest.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()> Public Class CDatabaseTest
+
+    <TestMethod()> Public Sub TestMethod1()
+        Dim getIn As New PrjDatabaseComponent.CDatabase
+        Dim vastus = getIn.connect()
+        Assert.IsTrue(vastus)
+    End Sub
+
+End Class
