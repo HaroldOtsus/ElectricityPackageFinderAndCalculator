318db65a77b6abff7886535a66edabe67af78b41
HaroldOtsus <haroldotsus@gmail.com>
2023-04-05 12:30:10 +0300

    Revert "APIComponent now asks for current time NordPool prices and filters only Estonian prices"
    This reverts commit 384378cde65579666d269196118802b3ab98ff17.

---
+++ Electricity_package_finder_and_calculator/userInterface.vb
@@ -0,0 +1,20 @@
+﻿Public Class userInterface
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        'Object that uses the API interface
+        Dim objekt As PrjAPIComponent.APIInterface
+
+        'Temporary object to use API component
+        objekt = New PrjAPIComponent.APIComponent
+
+        'Prints data from API component to text box
+        Dim result() As String = objekt.GetDataFromEleringAPI
+
+        'Filters to display only the price of string
+        For Each str As String In result
+            If Not String.IsNullOrEmpty(str) Then
+                TextBox1.AppendText(str.Substring(8, 7) & vbNewLine)
+            End If
+        Next
+    End Sub
+End Class
