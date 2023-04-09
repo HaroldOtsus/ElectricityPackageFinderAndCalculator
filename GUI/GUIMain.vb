271ad55b2cea38d12122189eeb91242abddb4b80
Klaabut <karlpaabut@gmail.com>
2023-04-09 22:15:20 +0300

    Small fix

---
+++ GUI/GUIMain.vb
@@ -0,0 +1,43 @@
+﻿Public Class GUIMain
+    Public sisend As String
+    Private Sub btnPackageHourlyRate_Click(sender As Object, e As EventArgs) Handles btnPackageHourlyRate.Click
+        TabControl1.SelectedTab = tabPackageHourlyRate
+    End Sub
+
+    Private Sub btnApplianceCalc_Click(sender As Object, e As EventArgs) Handles btnApplianceCalc.Click
+        TabControl1.SelectedTab = tabApplianceCalc
+    End Sub
+
+    Private Sub btnExchangePriceComparison_Click(sender As Object, e As EventArgs) Handles btnExchangePriceComparison.Click
+        TabControl1.SelectedTab = tabExchangeComparison
+    End Sub
+
+    Private Sub btnConsumptionHistory_Click(sender As Object, e As EventArgs) Handles btnConsumptionHistory.Click
+        TabControl1.SelectedTab = tabConsumptionHistory
+    End Sub
+
+    Private Sub btnComparePackages_Click(sender As Object, e As EventArgs) Handles btnPackageComparison.Click
+        TabControl1.SelectedTab = tabPackageComparison
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack0.Click, btnBack1.Click, btnBack2.Click, btnBack3.Click, btnBack4.Click
+        TabControl1.SelectedTab = Main
+    End Sub
+
+    Private Sub btnClientConsumptionHistory_Click(sender As Object, e As EventArgs) Handles btnClientConsumptionHistory.Click
+        TabControl2.SelectedTab = tabClientConsumptionHistory
+
+    End Sub
+
+    Private Sub btnSimulateConsumptionHistory_Click(sender As Object, e As EventArgs) Handles btnSimulateConsumptionHistory.Click
+        TabControl2.SelectedTab = tabSimulateExchangeHistory
+    End Sub
+
+
+    'If you change TabControl1 tab then TabControl2 reverts to a blank tab, looks cleaner this way IMO.
+    Private Sub tabConsumptionHistory_Leave(sender As Object, e As EventArgs) Handles tabConsumptionHistory.Leave
+        TabControl2.SelectedTab = tabBlank
+    End Sub
+
+
+End Class
