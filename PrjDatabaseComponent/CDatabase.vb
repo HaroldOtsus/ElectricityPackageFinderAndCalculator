3f7483ec4d1ce630924a6da2456c7767aa9d48d4
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-04-10 13:26:53 +0300

    working on function stockPrice

--- PrjDatabaseComponent/CDatabase.vb
+++ PrjDatabaseComponent/CDatabase.vb
@@ -4,12 +4,12 @@ Public Class CDatabase
     Implements IDatabase
     Implements IDatabaseAPI
 
-    Function test() As String()
-        '' Dim api As APIComponent.
-        '' api = New PrjAPIComponent.APIComponent
-        ''  Return api.GetDataFromEleringAPI
+    ''Function test() As String()
+    ''Dim api As APIComponent.APIInterface
+    ''  api = New APIComponent.APIComponent
+    ''Return api.GetDataFromEleringAPI
 
-    End Function
+    ''End Function
 
     Function stringReturn(ByVal id As String) As (consumptionPerHour As String, usageTime As String) Implements IDatabase.stringReturn
         Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;database=mydb;" 'string to access database
@@ -55,30 +55,27 @@ Public Class CDatabase
     End Function
 
 
-    Function stockPrice(ByRef one) As String
+    Function stockPrice() As String()
         Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;database=mydb;"
         Dim conn As New MySqlConnection(connString)
-        Dim dateOfStockPrices As String = ""
+        Dim dateOfStockPrices As String
         Dim dateToday
         dateToday = Date.Today 'get what date it is today
-
         Try
 
             conn.Open() 'try to connect to database
 
-            Dim command As New MySqlCommand("SELECT * FROM webdata WHERE idPacket = 1;", conn)
+            Dim command As New MySqlCommand("SELECT date FROM webdata WHERE idPacket = 1;", conn)
             Dim reader As MySqlDataReader = command.ExecuteReader()
             While reader.Read()
-                one = reader.GetString(1)
-                dateOfStockPrices = reader.GetString(26) 'get date from database
+                dateOfStockPrices = reader.GetString(0) 'get date from database
             End While
             If dateOfStockPrices = dateToday Then
-                Return dateOfStockPrices
+                ''need to return all prices from database
+                ''Return dateOfStockPrices
             Else
                 ''we ask prices from the API
                 ''we put the info to the database
-                one = ""
-                Return "sad"
             End If
             conn.Close()
         Catch ex As Exception
@@ -86,27 +83,52 @@ Public Class CDatabase
 
     End Function
 
-    Function insertStockPriceToDatabase(ByRef one As String, ByRef two As String, ByRef three As String) As String
-        Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;database=mydb;"
-        Dim conn As New MySqlConnection(connString)
-        Dim dateToday
-        dateToday = Date.Today 'get what date it is today
+    'Function insertStockPriceToDatabase() As String ''get data from API and insert it into database
+    '    Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;database=mydb;"
+    '    Dim conn As New MySqlConnection(connString)
+    '    Dim dateToday
+    '    dateToday = Date.Today 'get what date it is today
+    '    Dim api As APIComponent.APIInterface
+    '    api = New APIComponent.APIComponent
+    '    Dim sPrices As String()
+    '    sPrices = api.GetDataFromEleringAPI()
+    '    Try
 
-        Try
+    '        conn.Open() 'try to connect to database
 
-            conn.Open() 'try to connect to database
+    '        Dim command As New MySqlCommand("UPDATE webdata SET one = @colOne, two = @colTwo, three = @colThree, four = @colFour, five = @colFive, six = @colSix, seven = @colSeven, eight = @colEight, nine = @colNine, ten = @colTen, eleven = @colEleven, twelve = @col12, thirteen = @col13, fourteen = @col14, fifteen = @col15, sixteen = @col16, seventeen = @col17, eighteen = @col18, nineteen = @col19, twenty = @col20, twentyOne = @col21, twentyTwo = @col22, twentyThree = @col23, twentyFour = @col24, date = @colDate Where idPacket = 1 ", conn)
+    '        command.Parameters.AddWithValue("@colOne", sPrices(1))
+    '        command.Parameters.AddWithValue("@colTwo", sPrices(2))
+    '        command.Parameters.AddWithValue("@colThree", sPrices(3))
+    '        command.Parameters.AddWithValue("@colFour", sPrices(4))
+    '        command.Parameters.AddWithValue("@colFive", sPrices(5))
+    '        command.Parameters.AddWithValue("@colSix", sPrices(6))
+    '        command.Parameters.AddWithValue("@colSeven", sPrices(7))
+    '        command.Parameters.AddWithValue("@colEight", sPrices(8))
+    '        command.Parameters.AddWithValue("@colNine", sPrices(9))
+    '        command.Parameters.AddWithValue("@colTen", sPrices(10))
+    '        command.Parameters.AddWithValue("@colDate", dateToday)
+    '        command.Parameters.AddWithValue("@colEleven", sPrices(11))
+    '        command.Parameters.AddWithValue("@col12", sPrices(12))
+    '        command.Parameters.AddWithValue("@col13", sPrices(13))
+    '        command.Parameters.AddWithValue("@col14", sPrices(14))
+    '        command.Parameters.AddWithValue("@col15", sPrices(15))
+    '        command.Parameters.AddWithValue("@col16", sPrices(16))
+    '        command.Parameters.AddWithValue("@col17", sPrices(17))
+    '        command.Parameters.AddWithValue("@col18", sPrices(18))
+    '        command.Parameters.AddWithValue("@col19", sPrices(19))
+    '        command.Parameters.AddWithValue("@col20", sPrices(20))
+    '        command.Parameters.AddWithValue("@col21", sPrices(21))
+    '        command.Parameters.AddWithValue("@col22", sPrices(22))
+    '        command.Parameters.AddWithValue("@col23", sPrices(23))
+    '        command.Parameters.AddWithValue("@col24", sPrices(24))
+    '        command.ExecuteNonQuery()
+    '        conn.Close()
+    '        Return sPrices(11)
+    '    Catch ex As Exception
+    '    End Try
 
-            Dim command As New MySqlCommand("UPDATE webdata SET one = @colOne, two = @colTwo, three = @colThree, date = @colDate ", conn)
-            command.Parameters.AddWithValue("@colOne", one)
-            command.Parameters.AddWithValue("@colTwo", two)
-            command.Parameters.AddWithValue("@colThree", three)
-            command.Parameters.AddWithValue("@colDate", dateToday)
-            command.ExecuteNonQuery()
-            conn.Close()
-        Catch ex As Exception
-        End Try
-
-    End Function
+    'End Function
 
     Function Connect() As Boolean
         Dim connString As String = "server=84.50.131.222;user id=root;password=Koertelemeeldibjalutada!1;database=mydb;"
--- PrjDatabaseTestid/CDatabaseTest.vb
+++ PrjDatabaseTestid/CDatabaseTest.vb
@@ -5,14 +5,14 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 
 
-    <TestMethod()> Public Sub TestMethod1()
+    <TestMethod()> Public Sub TestConnection()
         Dim getIn As New PrjDatabaseComponent.CDatabase
         Dim vastus As String = getIn.Connect()
         Assert.IsTrue(vastus)
     End Sub
 
 
-    <TestMethod()> Public Sub TestMethod2()
+    <TestMethod()> Public Sub TestStringReturn()
         Dim getIn As New PrjDatabaseComponent.CDatabase
         Dim id As String
         id = "1"
@@ -20,21 +20,23 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
         Assert.AreEqual("1000", actualOutput.consumptionPerHour)
         Assert.AreEqual("5", actualOutput.usageTime)
     End Sub
-    <TestMethod()> Public Sub TestDatabaseInsert()
+    <TestMethod()> Public Sub TestDatabaseGet()
         Dim getIn As New PrjDatabaseComponent.CDatabase
-        Dim first As String = ""
-        getIn.stockPrice(first)
-        Assert.AreEqual("6,00", first)
+        Dim output = getIn.stockPrice()
+        Assert.AreEqual("cool", output)
     End Sub
 
-    <TestMethod()> Public Sub TestDatabaseInsertAPI()
-        Dim getIn As New PrjDatabaseComponent.CDatabase
-        Dim first As String = "6,00"
-        Dim second As String = "7,00"
-        Dim third As String = "5,00"
-        getIn.insertStockPriceToDatabase(first, second, third)
-        ''right now test doesn't show anything in visual studio but when running this data in databse changes so test is successful
-    End Sub
+
+
+    '<TestMethod()> Public Sub TestDatabaseInsertAPI()
+    '    Dim getIn As New PrjDatabaseComponent.CDatabase
+    '    Dim first As String = "6,00"
+    '    Dim second As String = "7,00"
+    '    Dim third As String
+    '    third = getIn.insertStockPriceToDatabase()
+    '    Assert.AreEqual("cool", third)
+    '    ''right now test doesn't show anything in visual studio but when running this data in databse changes so test is successful
+    'End Sub
 
 
 End Class
