b5c05a4e70696442e500bfd51f6296ee6d29d7f4
HaroldOtsus <haroldotsus@gmail.com>
2023-04-10 15:33:15 +0300

    Added function to APIComponent that gives NordPool prices depending on the dates given in format YYYY-MM-DD.

--- APIComponent/APIComponent.vb
+++ APIComponent/APIComponent.vb
@@ -55,4 +55,51 @@ Public Class APIComponent
         Return endResult
     End Function
 
+    Public Function GetDataFromEleringAPIWithDates(ByVal strStartDate As String, ByVal strEndDate As String) As String() _
+        Implements APIInterface.GetDataFromEleringAPIWithDates
+
+#Disable Warning BC42025 ' Access of shared member, constant member, enum member or nested type through an instance
+
+        'HttpWebRequest object sends a request to url specified
+        Dim webRequest As HttpWebRequest = CType(webRequest.Create("https://dashboard.elering.ee/api/nps/price?start=" + strStartDate _
+        + "T20%3A59%3A59.999Z&end=" + strEndDate + "T20%3A59%3A59.999Z"), HttpWebRequest)
+
+#Enable Warning BC42025 ' Access of shared member, constant member, enum member or nested type through an instance
+
+        'GET method means that we are retrieveing data from web server
+        webRequest.Method = "GET"
+        'Data received is in JSON format
+        webRequest.ContentType = "application/json"
+
+        'webRequest.GetResponse() recieves a response from web server and CType changes it to HttpWebResponse object
+        'This allows us to access properties and methods to get information about the response from the server
+        Dim webResponse As HttpWebResponse = CType(webRequest.GetResponse(), HttpWebResponse)
+        'responseStream contains the data stream sent by the server
+        Dim responseStream As System.IO.Stream = webResponse.GetResponseStream()
+        'Allows us to read the data sent by the server as a string of characters
+        Dim stringStreamReader As System.IO.StreamReader = New StreamReader(responseStream)
+        'Reads all the characters from stringStreamReader and stores them in a string and
+        'the variable responseString contains that string
+        Dim responseString As String = stringStreamReader.ReadToEnd.ToString
+        'Frees the resources used by HttpWebRequest object
+        webResponse.Close()
+        'Splits the web response string into multiple strings separated by commas
+        Dim result() As String = responseString.Split(","c)
+        Dim endResult(0) As String
+
+        'Filters only strings that have the "price string" and only Estonian prices
+        For Each str As String In result
+            If str.Contains("price") Then
+                ReDim Preserve endResult(endResult.Length)
+                str = str.Substring(8)
+                str = str.Replace("]", "")
+                str = str.Replace("}", "")
+                endResult(endResult.Length - 1) = str
+            ElseIf str.Contains("fi") Then
+                Exit For
+            End If
+        Next
+
+        Return endResult
+    End Function
 End Class
--- APIComponent/APIInterface.vb
+++ APIComponent/APIInterface.vb
@@ -4,5 +4,8 @@
     '1 string contains one price at that hour, based on the order of strings
     'First string is always the earliest price and last string is the latest price
     Function GetDataFromEleringAPI() As String()
-
+    'Function returns 24 strings in an array and takes in 2 strings that should be in date format "yyyy-MM-DD"
+    '1 string contains one price at that hour, based on the order of strings
+    'First string is always the earliest price and last string is the latest price
+    Function GetDataFromEleringAPIWithDates(ByVal strStartDate As String, ByVal strEndDate As String) As String()
 End Interface
