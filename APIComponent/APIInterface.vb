00f2b1a1565141507cc23d917822a7c6ce71ca04
HaroldOtsus <haroldotsus@gmail.com>
2023-04-11 10:28:32 +0300

    C

--- APIComponent/APIInterface.vb
+++ APIComponent/APIInterface.vb
@@ -2,7 +2,7 @@
 
     'Function returns 24 strings in an array
     '1 string contains one price at that hour, based on the order of strings
-    'First string is always the earliest price and last string is the latest price
+    'First string is always the earliest price and last string is the latest price 
     Function GetDataFromEleringAPI() As String()
     'Function returns 24 strings in an array and takes in 2 strings that have to be in date format "YYYY-MM-DD"
     '1 string contains one price at that hour, based on the order of strings
