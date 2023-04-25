b8971cc9737a0ff5e09e3ba1bc5c9083ebb845f8
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-04-25 23:03:56 +0300

    added comments

--- GUI/GUIMain.vb
+++ GUI/GUIMain.vb
@@ -1130,9 +1130,9 @@ Public Class GUIMain
     End Sub
 
     Private Function packageChartOfElectricity()
-        chartPackages.Series.Clear()
+        chartPackages.Series.Clear() 'clear chart
         Dim returnString As PrjDatabaseComponent.IDatabase
-        returnString = New PrjDatabaseComponent.CDatabase
+        returnString = New PrjDatabaseComponent.CDatabase 'use database component
         Dim count As Integer
         count = returnString.electricityPackagesCount 'find out how many packages there are
         Dim packages = returnString.electricityPackagesInfo ' get info about packages
@@ -1162,15 +1162,15 @@ Public Class GUIMain
             series.ChartType = DataVisualization.Charting.SeriesChartType.StepLine
             Dim r As Integer = rand.Next(0, 256) 'create random red value
             Dim g As Integer = rand.Next(0, 256) 'green value
-            Dim b As Integer = rand.Next(0, 256) 'blue val
+            Dim b As Integer = rand.Next(0, 256) 'blue value
             Dim colorofLine As Color = Color.FromArgb(r, g, b) 'random color
             series.Color = colorofLine
             series.BorderWidth = 3
             'all the packages are right except Muutuv 
-            Dim currentDate As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
-            Dim futureDate As DateTime = DateTime.Now.AddHours(24)
+            Dim currentDate As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") 'current date
+            Dim futureDate As DateTime = DateTime.Now.AddHours(24) 'date 24h from now
             Dim futureDateString As String = futureDate.ToString("yyyy-MM-dd HH:mm:ss")
-            Dim data = returnString.getStockPriceAndDatesFromDatabaseFuture(currentDate, futureDateString)
+            Dim data = returnString.getStockPriceAndDatesFromDatabaseFuture(currentDate, futureDateString) 'get stock prices and date from database
             Dim hour2(24) As Integer
             Dim dateFromUnix(24) As String
             For k As Integer = 1 To 24
@@ -1180,21 +1180,21 @@ Public Class GUIMain
                 'dateFromUnix(k) = dateTime.ToString("yyyy-MM-dd HH:mm:ss")
                 'hour2(k) = dateTime.Hour
                 Dim oDate As DateTime = Convert.ToDateTime(data.Item2(k))
-                hour2(k) = oDate.Hour
+                hour2(k) = oDate.Hour 'get hour from 
             Next
-            If Not packages.Item5(j) Then
-                If packages.Item7(j) = True Then
+            If Not packages.Item5(j) Then 'is the package price tied to market price?
+                If packages.Item7(j) = True Then 'if package has different night and day prices
                     For i As Integer = 1 To 24
 
-                        If hour2(i) > 11 And hour2(i) < 24 Then
+                        If hour2(i) > 11 And hour2(i) < 24 Then 'day price
                             series.Points.AddXY(data.Item2(i), packages.Item3(j))
                         Else
-                            series.Points.AddXY(data.Item2(i), packages.Item8(j))
+                            series.Points.AddXY(data.Item2(i), packages.Item8(j)) 'night price
                         End If
                     Next
 
                 Else
-                    For i As Integer = 1 To 24
+                    For i As Integer = 1 To 24 'package has fixed price
 
 
                         series.Points.AddXY(data.Item2(i), packages.Item3(j))
@@ -1206,11 +1206,11 @@ Public Class GUIMain
 
 
 
-                For i As Integer = 1 To 24
+                For i As Integer = 1 To 24 'package is tied to market price
                     ' Dim dateTimeOffset As DateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(data.Item2(i)) 'new datetimeoffset from sDate string
                     'Dim dateValue As Date = dateTimeOffset.LocalDateTime 'convert to date
                     Dim oDate As DateTime = Convert.ToDateTime(data.Item2(i))
-                    Dim hour As Integer = oDate.Hour
+                    Dim hour As Integer = oDate.Hour 'get hour from string
                     Dim price As Double
                     Dim culture As System.Globalization.CultureInfo = System.Globalization.CultureInfo.InstalledUICulture
                     Dim language As String = culture.TwoLetterISOLanguageName ' find out language of windows op
@@ -1219,18 +1219,18 @@ Public Class GUIMain
                         'packages.Item3(j) = packages.Item3(j).Replace(".-", ",")
                     End If
                     Dim pricesD As Double = Double.Parse(data.Item1(i))
-                    pricesD = (pricesD / 1000) * 100
+                    pricesD = (pricesD / 1000) * 100 'MWH/eur to kWh/s
                     price = pricesD + packages.Item3(j)
 
                     ' series.Points.AddXY(hour, price)
-                    chartPackages.Series(j).Points.AddXY(data.Item2(i), price)
+                    chartPackages.Series(j).Points.AddXY(data.Item2(i), price) 'line
 
                 Next
             End If
         Next
     End Function
     Private Sub btnPackets_Click(sender As Object, e As EventArgs) Handles btnPackets.Click
-        packageChartOfElectricity()
+        packageChartOfElectricity() 'create chart for packets
     End Sub
 
     Private Sub btnTableDesc_Click(sender As Object, e As EventArgs) Handles btnTableDesc.Click
@@ -1610,20 +1610,20 @@ Public Class GUIMain
     End Sub
 
     Private Sub btnTwoPackets_Click(sender As Object, e As EventArgs) Handles btnTwoPackets.Click
-        Dim packet1 As String = cBoxPackage1.Text
+        Dim packet1 As String = cBoxPackage1.Text 'get packet name
         Dim packet2 As String = cBoxPackage2.Text
-        Dim result As Integer = StrComp(packet1, packet2, 0)
-        If result <> 0 Then
+        Dim result As Integer = StrComp(packet1, packet2, 0) 'check if strings are same
+        If result <> 0 Then ' if strings are not same
             Dim returnString As PrjDatabaseComponent.IDatabase
             returnString = New PrjDatabaseComponent.CDatabase
-            Dim packageone = returnString.onePackageInfo(packet1)
+            Dim packageone = returnString.onePackageInfo(packet1) ' get info about packages from database
             Dim packagetwo = returnString.onePackageInfo(packet2)
-            compOne.Text = packageone.Item2
+            compOne.Text = packageone.Item2 'company name
             compTwo.Text = packagetwo.Item2
-            priceOfCont.Text = packageone.Item4
+            priceOfCont.Text = packageone.Item4 'price of the contract
             priceOfCont2.Text = packagetwo.Item4
 
-            chartPackages.Series.Clear()
+            chartPackages.Series.Clear() 'clear chart
 
             chartPackages.Width = 600 ' set chart the width to 600 pixels
             chartPackages.Height = 400 'set chart height to 400 lines
@@ -1643,30 +1643,29 @@ Public Class GUIMain
             chartPackages.ChartAreas(0).AxisX.ScrollBar.IsPositionedInside = True
             chartPackages.ChartAreas(0).AxisY.ScrollBar.IsPositionedInside = True
             Dim loopThroughBothPackets As Integer = 0
-            While loopThroughBothPackets < 2
+            While loopThroughBothPackets < 2 'loop through both packages
                 Dim series As New Series(packageone.Item1) ' create a new series with the package name
                 chartPackages.Series.Add(series)
                 series.ChartType = DataVisualization.Charting.SeriesChartType.StepLine
                 series.BorderWidth = 3
-                'all the packages are right except Muutuv 
                 Dim currentDate As String = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
                 Dim futureDate As DateTime = DateTime.Now.AddHours(24)
                 Dim futureDateString As String = futureDate.ToString("yyyy-MM-dd HH:mm:ss")
-                Dim data = returnString.getStockPriceAndDatesFromDatabaseFuture(currentDate, futureDateString)
+                Dim data = returnString.getStockPriceAndDatesFromDatabaseFuture(currentDate, futureDateString) 'get stock prices and dates from database
                 Dim hour2(24) As Integer
                 Dim dateFromUnix(24) As String
                 For k As Integer = 1 To 24
                     Dim oDate As DateTime = Convert.ToDateTime(data.Item2(k))
-                    hour2(k) = oDate.Hour
+                    hour2(k) = oDate.Hour 'get hour form stock market price
                 Next
-                If Not packageone.Item5 Then
-                    If packagetwo.Item7 = True Then
+                If Not packageone.Item5 Then 'if packet does not use market price
+                    If packagetwo.Item7 = True Then 'is the packet has different night price
                         For i As Integer = 1 To 24
 
-                            If hour2(i) > 11 And hour2(i) < 24 Then
+                            If hour2(i) > 11 And hour2(i) < 24 Then 'day
                                 series.Points.AddXY(data.Item2(i), packageone.Item3)
                             Else
-                                series.Points.AddXY(data.Item2(i), packageone.Item8)
+                                series.Points.AddXY(data.Item2(i), packageone.Item8) 'night
                             End If
                         Next
 
@@ -1674,7 +1673,7 @@ Public Class GUIMain
                         For i As Integer = 1 To 24
 
 
-                            series.Points.AddXY(data.Item2(i), packageone.Item3)
+                            series.Points.AddXY(data.Item2(i), packageone.Item3) 'packet has fixed price
 
 
                         Next
@@ -1683,7 +1682,7 @@ Public Class GUIMain
 
 
 
-                    For i As Integer = 1 To 24
+                    For i As Integer = 1 To 24 'packet si tied to market price
                         'Dim dateTimeOffset As DateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(data.Item2(i)) 'new datetimeoffset from sDate string
                         'Dim dateValue As Date = dateTimeOffset.LocalDateTime 'convert to date
                         Dim oDate As DateTime = Convert.ToDateTime(data.Item2(i))
@@ -1696,10 +1695,11 @@ Public Class GUIMain
                             'packages.Item3(j) = packages.Item3(j).Replace(".-", ",")
                         End If
                         Dim pricesD As Double = Double.Parse(data.Item1(i))
-                        price = pricesD + packageone.Item3
+                        pricesD = (pricesD / 1000) * 100 'MWH/eur to kWh/s
+                        price = pricesD + packageone.Item3 'add marginal
 
                         ' series.Points.AddXY(hour, price)
-                        chartPackages.Series(loopThroughBothPackets).Points.AddXY(data.Item2(i), price)
+                        chartPackages.Series(loopThroughBothPackets).Points.AddXY(data.Item2(i), price) 'add line to market
 
                     Next
                 End If
