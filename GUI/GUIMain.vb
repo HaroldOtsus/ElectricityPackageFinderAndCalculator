aad6d50a7e9a9dd906676a7bff37a97d9fbf7534
tempestcannoli <92439001+tempestcannoli@users.noreply.github.com>
2023-05-05 18:59:23 +0300

    added component and function csv and IT WORKS

--- GUI/GUIMain.vb
+++ GUI/GUIMain.vb
@@ -2343,5 +2343,113 @@ Public Class GUIMain
         Next
     End Sub
 
+    Private Sub btnImportCSVFileSimu_Click(sender As Object, e As EventArgs) Handles btnImportCSVFileSimu.Click
+        'tblCSVfile.Controls.Clear()
+        'chrtCSV.Series.Clear()
 
+        'opens a window for user to select their CSV file
+        Dim openFileDialog As New OpenFileDialog()
+        'Filter to only show CSV files and all files
+        openFileDialog.Filter = "CSV Files (*.csv)|*.csv|All Files (*.*)|*.*"
+        'If the user selects a file and presses OK
+        'Dim selectedFileName As String = openFileDialog.FileName
+
+        Dim returntable As CCSVReader.ICSVReader
+        If openFileDialog.ShowDialog() = DialogResult.OK Then
+
+            Dim selectedFileName As String = openFileDialog.FileName
+
+
+            tableOfCSV = CCSVReader.CCSVReader.ReadCSV(selectedFileName)
+            tbDebug.Clear()
+            Dim sumKWh As Double
+            Dim sumPrice As Double
+            Dim kWh As Double
+            Dim price As Double
+
+            'if the table fits the template(is correct)
+            If tableOfCSV.Columns(0).ColumnName = "Algus" And tableOfCSV.Columns(2).ColumnName = "Kogus (kWh)" _
+                            And tableOfCSV.Columns(1).ColumnName = "Lõpp" And tableOfCSV.Columns(3).ColumnName = "Börsihind (EUR / MWh)" Then
+                Dim divider As Integer = 0
+                For Each row As DataRow In tableOfCSV.Rows
+                    'ADD UP ALL THE QUANTITY(kWh)
+                    If IsNumeric(row("Kogus (kWh)")) Then
+                        kWh = Double.Parse(row("Kogus (kWh)"))
+                    Else
+                        kWh = 0
+                    End If
+
+                    'ADD UP ALL THE PRICES
+                    If IsNumeric(row("Börsihind (EUR / MWh)")) Then
+                        price = Double.Parse(row("Börsihind (EUR / MWh)"))
+                    Else
+                        price = 0
+                    End If
+                    'price = Double.Parse(row("Börsihind (EUR / MWh)"))
+                    tbDebug.AppendText(Environment.NewLine & row("Algus") & " " & row("Lõpp") & " " & kWh & " kWh" & " " & price & " €/mWh")
+                    'tbDebug.Text = sumKWh & sumPrice
+                    sumKWh += kWh
+                    sumPrice += price
+                    divider += 1
+                Next
+                tbDebug.AppendText(Environment.NewLine & sumKWh)
+                'date time pickers are enabled
+                dtpBeginning.Enabled = True
+                dtpEnd.Enabled = True
+
+                'length of table
+                Dim rowCount As Integer = tableOfCSV.Rows.Count
+                Dim rowCountInForEach As Integer = 0
+
+
+
+                'For i As Integer = 0 To 9
+                For Each row As DataRow In tableOfCSV.Rows
+                    'if there are too many rows of data take only the first 3 days
+                    'If rowCountInForEach = (24 * 3) + 1 Then
+                    '    Exit For
+                    'End If
+                    'Dim row As DataRow = table.Rows(i)
+
+                    'if current row is the first row then set minDate for  dtpBeginning
+                    If rowCountInForEach = 0 Then
+                        dtpBeginning.MinDate = DateTime.Parse(row("Algus"))
+                        dtpBeginning.Value = DateTime.Parse(row("Algus"))
+                        'MsgBox("minDate for  dtpBeginning" & row("Algus"))
+                    End If
+                    'if current row is the second row in the table then set minDate for  dtpEnd
+                    If rowCountInForEach = 1 Then
+                        dtpEnd.MinDate = DateTime.Parse(row("Lõpp"))
+                        dtpEnd.Value = DateTime.Parse(row("Lõpp"))
+                        'MsgBox("minDate for  dtpEnd" & row("Lõpp"))
+                    End If
+                    'if current row count is the row BEFORE the last row then set maxDate for dtpBeginning
+
+                    If rowCountInForEach = rowCount - 2 Then 'has to be -2 because rowCountInForEach starts off as 0
+                        dtpBeginning.MaxDate = DateTime.Parse(row("Algus"))
+                        'MsgBox("maxDate for dtpBeginning" & row("Algus"))
+                    End If
+                    'if current row is the last row in the table then set maxDate for dtpEnd
+                    If row Is tableOfCSV.Rows(rowCount - 1) Then
+                        dtpEnd.MaxDate = DateTime.Parse(row("Lõpp"))
+                        'MsgBox("maxDate for dtpEnd" & row("Lõpp"))
+                    End If
+
+
+                    '
+                    rowCountInForEach += 1
+                Next
+                'MsgBox(rowCountInForEach)
+                'sets the minimum and max dates that can be selected
+                dtpBeginning.Value = dtpBeginning.MinDate
+                dtpEnd.Value = dtpEnd.MaxDate
+
+            Else
+                MessageBox.Show("VALE FORMAAT!")
+            End If
+        End If
+
+
+
+    End Sub
 End Class
